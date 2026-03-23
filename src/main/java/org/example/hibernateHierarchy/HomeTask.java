76c5a79e2be235b3499a4875f74ef4749bd7b3ca
System Administrator <flame5930@gmail.com>
2026-03-23 10:26:36 +0300

    временный коммит2

--- src/main/java/org/example/hibernateHierarchy/HomeTask.java
+++ src/main/java/org/example/hibernateHierarchy/HomeTask.java
@@ -1,6 +1,14 @@
 package org.example.hibernateHierarchy;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Data;
+
 import java.util.Date;
+
+@Data
+@Entity
+@DiscriminatorValue("HomeTask")
 public class HomeTask extends Task{
     Date startDate;
     Date endDate;
--- src/main/java/org/example/hibernateHierarchy/Task.java
+++ src/main/java/org/example/hibernateHierarchy/Task.java
@@ -1,7 +1,23 @@
 package org.example.hibernateHierarchy;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
 @Entity
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+/*@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "task_type")*/
 public class Task {
-    Integer id;
+
+    @Id
+    @GeneratedValue
+    private Long id;
     String name;
     String description;
 
